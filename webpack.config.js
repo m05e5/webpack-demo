5dda20bf0db878e9f6465daadefe0f26005292d1
m05e5 <calebmoses1111@gmail.com>
2021-07-13 11:05:47 +0100

    set up HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ module.exports = {
+   entry: {
+     index: './src/index.js',
+   },
+   devtool: 'inline-source-map',
+   devServer: {
+    contentBase: './dist',
   },
-};
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+ };
